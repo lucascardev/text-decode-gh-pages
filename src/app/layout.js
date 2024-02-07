ca83d025adfe8c17abc7c8cfd48d03f17cba60b7
lucascardev <lucasmatheussc97@gmail.com>
2024-02-07 14:36:31 -0300

    changes on title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Decodificador de Texto  ",
+  title: "Decodificador de Texto",
   description: "Criptografia de texto",
 };
 
