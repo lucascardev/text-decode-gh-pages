bb359652c5a824f94748e78baac35e47addc21ba
lucascardev <lucasmatheussc97@gmail.com>
2024-02-16 19:28:07 -0300

    changes for images

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,44 @@
 'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
-import vector from './assets/vector.png'
+import vector from '../../public/vector.png'
 import { useState } from 'react'
 import Button from './components/Button'
-import { FiCopy } from "react-icons/fi";
+import { FiCopy } from 'react-icons/fi'
 import Footer from './components/Footer'
-import alura_logo from './assets/aluravector.png'
+import alura_logo from '../../public/aluravector.png'
 
 export default function Home() {
 	const [message, setMessage] = useState('')
 
 	const [encryptedMessage, setEncryptedMessage] = useState('')
-  const [messageStatus, setMessageStatus] = useState('')
+	const [messageStatus, setMessageStatus] = useState('')
 
 	const [passwordKey, setPasswordKey] = useState('')
 
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(encryptedMessage);
-      alert('Mensagem copiada para a área de transferência!');
-    } catch (err) {
-      console.log('Falha ao copiar o texto', err);
-    }
-  };
+	const copyToClipboard = async () => {
+		try {
+			await navigator.clipboard.writeText(encryptedMessage)
+			alert('Mensagem copiada para a área de transferência!')
+		} catch (err) {
+			console.log('Falha ao copiar o texto', err)
+		}
+	}
 
 	return (
 		<main className={styles.main}>
-    
-      <Image 
-      src={alura_logo}
-      width={20}
-      height={25}
-      className={styles.aluraImagem}
-      />
-    
+			<Image
+				src={alura_logo}
+				width={20}
+				height={25}
+				className={styles.aluraImagem}
+			/>
+
 			<section className={styles.inputSection}>
 				<textarea
-        autoFocus
+					autoFocus
 					className={styles.textArea}
-          onChange={(e) => setMessage(e.target.value)}
+					onChange={(e) => setMessage(e.target.value)}
 					value={message}
 					placeholder='Digite seu texto aqui...'
 				/>
@@ -52,8 +51,8 @@ export default function Home() {
 							id='password_input'
 							type='password'
 							className={styles.password_input}
-              placeholder='Senha de criptografia'
-              value={passwordKey}
+							placeholder='Senha de criptografia'
+							value={passwordKey}
 							onChange={(e) => setPasswordKey(e.target.value)}
 						/>
 					</form>
@@ -65,8 +64,8 @@ export default function Home() {
 						message={message}
 						req_password={passwordKey}
 						setEncryptedMessage={setEncryptedMessage}
-            setMessage={setMessage}
-            setMessageStatus={setMessageStatus}
+						setMessage={setMessage}
+						setMessageStatus={setMessageStatus}
 					/>
 					<Button
 						name='Descriptografar'
@@ -75,10 +74,10 @@ export default function Home() {
 						req_password={passwordKey}
 						setEncryptedMessage={setEncryptedMessage}
 						setMessage={setMessage}
-            setMessageStatus={setMessageStatus}
+						setMessageStatus={setMessageStatus}
 					/>
 				</div>
-        <Footer />
+				<Footer />
 			</section>
 			<section className={styles.asideSection}>
 				{encryptedMessage == '' ? (
@@ -86,15 +85,23 @@ export default function Home() {
 						<Image src={vector} className={styles.vector} />
 						<h3>Nenhuma mensagem encontrada.</h3>
 						<p>
-							Digite, ao lado, um texto que você deseja criptografar ou
-							descriptografar.
+							Digite, ao lado, um texto que você deseja
+							criptografar ou descriptografar.
 						</p>
 					</div>
 				) : (
 					<div className={styles.messageCard}>
-            <span className={styles.messageCardSpan}>Mensagem {messageStatus === "encrypted" ? "encriptografada" : "desencriptografada"} :</span>
-					<p className={styles.messageText}>{encryptedMessage}</p>
-          <button onClick={copyToClipboard}><FiCopy /></button>
+						<span className={styles.messageCardSpan}>
+							Mensagem{' '}
+							{messageStatus === 'encrypted'
+								? 'encriptografada'
+								: 'desencriptografada'}{' '}
+							:
+						</span>
+						<p className={styles.messageText}>{encryptedMessage}</p>
+						<button onClick={copyToClipboard}>
+							<FiCopy />
+						</button>
 					</div>
 				)}
 			</section>
