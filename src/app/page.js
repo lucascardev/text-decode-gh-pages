6b81d8874e65dae0c9fe5b38bfdbd0c648a2ed0f
lucascardev <lucasmatheussc97@gmail.com>
2024-02-16 20:02:30 -0300

    changes for image

--- src/app/page.js
+++ src/app/page.js
@@ -82,7 +82,7 @@ export default function Home() {
 			<section className={styles.asideSection}>
 				{encryptedMessage == '' ? (
 					<div className={styles.no_messages_display}>
-						<Image src={'./vector.png'} className={styles.vector} />
+						<Image src="/text-decode-gh-pages/vector.png" className={styles.vector} />
 						<h3>Nenhuma mensagem encontrada.</h3>
 						<p>
 							Digite, ao lado, um texto que você deseja
